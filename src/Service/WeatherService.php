ecfeab2d8805e9f44a5f39219cfda3af66187d42
ivankvkharkiv <ivankvkharkiv@gmail.com>
2025-04-14 01:26:36 +0300

    added logging system

--- src/Service/WeatherService.php
+++ src/Service/WeatherService.php
@@ -5,13 +5,21 @@ namespace App\Service;
 use App\Assembler\WeatherDataAssembler;
 use App\Client\WeatherClient;
 use App\Dto\WeatherResultDto;
+use Psr\Log\LoggerInterface;
 
 class WeatherService
 {
-    public function __construct(private WeatherClient $weatherClient) {}
+    public function __construct(
+        private WeatherClient $weatherClient,
+        private LoggerInterface $logger
+    ) {}
 
     public function getWeather(string $cityName): WeatherResultDto
     {
-        return WeatherDataAssembler::toDto($this->weatherClient->getWeather($cityName));
+        $weatherResultDto = WeatherDataAssembler::toDto($this->weatherClient->getWeather($cityName));
+
+        $this->logger->emergency("Weather result for {$cityName}.", ['weatherData'=> $weatherResultDto]);
+
+        return $weatherResultDto;
     }
 }
