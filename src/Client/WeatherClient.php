d23eee05e75a4283ee28150db40ff326f92d26a2
ivankvkharkiv <ivankvkharkiv@gmail.com>
2025-04-13 17:51:55 +0300

    added weather client, php-cs-fixer, env data

---
+++ src/Client/WeatherClient.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Client;
+
+use App\Exception\WeatherResponseException;
+use Symfony\Component\HttpClient\HttpOptions;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class WeatherClient
+{
+    private HttpClientInterface $client;
+
+    public function __construct(
+        string $apiEndPoint,
+        private string $apiKey,
+        HttpClientInterface $httpClient,
+    ) {
+        $this->client = $httpClient->withOptions(
+            (new HttpOptions())
+                ->setBaseUri($apiEndPoint)
+                ->toArray()
+        );
+    }
+
+    public function getWeather(string $city): array
+    {
+        $response = $this->client->request('GET', "/v1/current.json?key={$this->apiKey}&q={$city}");
+
+        if ($response->getStatusCode() < 200 || $response->getStatusCode() > 299) {
+            throw new WeatherResponseException(sprintf('Weather endpoint wrong status code: %s', $response->getStatusCode(false)));
+        }
+
+        $weather = json_decode($response->getContent(), true);
+
+        if (isset($weather['error'])) {
+            throw new WeatherResponseException($weather['error']['message']);
+        }
+
+        return $weather;
+    }
+}
---
+++ tests/Application/WeatherClientTest.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Tests\Application;
+
+use App\Client\WeatherClient;
+use App\Exception\WeatherResponseException;
+use PHPUnit\Framework\Attributes\Test;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\MockResponse;
+use Symfony\Component\HttpClient\TraceableHttpClient;
+use function PHPUnit\Framework\assertEqualsCanonicalizing;
+
+final class WeatherClientTest extends KernelTestCase
+{
+    #[Test]
+    public function itShouldGetWeatherInfoFromEndPoint(): void
+    {
+        // Arrange
+        $weatherResponse = [
+            'location' => ['name' => 'Madrid', 'country' => 'Spain'],
+            'current' => [
+                'temp_c' => 12,
+                'condition' => ['text' => 'condition'],
+                'humidity' => '80%',
+                'wind_kph' => 25,
+                'last_updated' => '2025-04-13 17:16:18',
+            ],
+        ];
+        $mockResponseJson = json_encode($weatherResponse, JSON_THROW_ON_ERROR);
+
+        $mockResponse = new MockResponse($mockResponseJson, [
+            'http_code' => 201,
+            'response_headers' => ['Content-Type: application/json'],
+        ]);
+
+        $httpClient = new MockHttpClient($mockResponse, 'https://example.com');
+
+        self::getContainer()->set('Symfony\Contracts\HttpClient\HttpClientInterface', new TraceableHttpClient($httpClient));
+
+        $weatherClient = self::getContainer()->get(WeatherClient::class);
+
+        // Act
+        $clientResponse = $weatherClient->getWeather('Madrid');
+
+        // Assert
+        $this->assertSame('GET', $mockResponse->getRequestMethod());
+        $this->assertSame('https://api.weather_test_endpoint.com/v1/current.json?key=weather_test_endpoint_key&q=Madrid', $mockResponse->getRequestUrl());
+        assertEqualsCanonicalizing($clientResponse, $weatherResponse);
+    }
+
+    #[Test]
+    public function itShouldThrowWeatherResponseException(): void
+    {
+        // Arrange
+        $weatherResponse = [
+            'error' => ['message' => 'Error'],
+        ];
+
+        $mockResponseJson = json_encode($weatherResponse, JSON_THROW_ON_ERROR);
+
+        $mockResponse = new MockResponse($mockResponseJson, [
+            'http_code' => 201,
+            'response_headers' => ['Content-Type: application/json'],
+        ]);
+
+        $httpClient = new MockHttpClient($mockResponse, 'https://example.com');
+
+        self::getContainer()->set('Symfony\Contracts\HttpClient\HttpClientInterface', new TraceableHttpClient($httpClient));
+
+        $weatherClient = self::getContainer()->get(WeatherClient::class);
+
+        // Act
+        $this->expectException(WeatherResponseException::class);
+        $weatherClient->getWeather('Madrid');
+
+        // Assert
+        $this->assertSame('GET', $mockResponse->getRequestMethod());
+        $this->assertSame('https://api.weather_test_endpoint.com/v1/current.json?key=weather_test_endpoint_key&q=Madrid', $mockResponse->getRequestUrl());
+    }
+
+    #[Test]
+    public function itShouldThrowWeatherResponseExceptionOnHttpCode(): void
+    {
+        // Arrange
+        $weatherResponse = [
+        ];
+
+        $mockResponseJson = json_encode($weatherResponse, JSON_THROW_ON_ERROR);
+
+        $mockResponse = new MockResponse($mockResponseJson, [
+            'http_code' => 403,
+            'response_headers' => ['Content-Type: application/json'],
+        ]);
+
+        $httpClient = new MockHttpClient($mockResponse, 'https://example.com');
+
+        self::getContainer()->set('Symfony\Contracts\HttpClient\HttpClientInterface', new TraceableHttpClient($httpClient));
+
+        $weatherClient = self::getContainer()->get(WeatherClient::class);
+
+        // Act
+        $this->expectException(WeatherResponseException::class);
+        $weatherClient->getWeather('Madrid');
+
+        // Assert
+        $this->assertSame('GET', $mockResponse->getRequestMethod());
+        $this->assertSame('https://api.weather_test_endpoint.com/v1/current.json?key=weather_test_endpoint_key&q=Madrid', $mockResponse->getRequestUrl());
+    }
+}
